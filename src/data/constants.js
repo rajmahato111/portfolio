9d317a21bca03889405b7e26e64067fb911c2a6c
Raj Kumar Mahto <rajmahato111@gmail.com>
2024-09-27 10:15:31 -0700

    initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,338 @@
+export const Bio = {
+  name: "Raj Kumar Mahto",
+  roles: [
+    "Backend Developer",
+    "Node JS Developer",
+    "Java Developer",
+    "Python Developer"
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/rajmahato111",
+  resume: "https://drive.google.com/file/d/1E6jdU0Ow5TdXdTfjEdwKhIvNunSexlcj/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/rajmahato111/",
+  twitter: "https://x.com/rajmahato111",
+  insta: "https://www.instagram.com/rajmahato111/",
+  facebook: "https://www.facebook.com/rajmahato111/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React.js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Next.js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "TypeScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/768px-Typescript_logo_2020.svg.png",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/tailwind-css-icon.png",
+      },
+      {
+        name: "HTML5",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS3",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "GraphQL",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/GraphQL_Logo.svg/2048px-GraphQL_Logo.svg.png",
+      },
+      {
+        name: "React Testing Library",
+        image: "https://testing-library.com/img/octopus-64x64.png",
+      },
+      {
+        name: "Jest",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAbFBMVEVHcEzfxMz////////////s2+D////////8+fr//v7z5+v+/P3//f7//v///////////////////////v7///////////////+aQ1yYPliVOFSydIXUtLyjVmyraXvq2d7InanAkJ25g5KSLU3YYNw1AAAAGHRSTlMA/a7Ab+kbKPf89+PN1wsvm4gDTlQ3QF9D6eTdAAABW0lEQVQokY1S2Y6DMAzMVchFKdB2E+ck/P8/bhClB33YHUXKw8j2zNgI/Q9N+45m/CCZOr0gh/MHeZWz21ESO7SlBewGcPp2GHrxxhi7PpvJeCAnFawNOVpr8KErQqMuALPIxsau/TIz4CVgfYqQybfTG4dCbyQv8udD6fZ1Mx7Qz8nxN5PtQMmE0Jlx3vXorKW43F8ecPG6qSXzLK/ornDhl53rtQNzYohka7PgnAcoanpGM9sgpzWE4HHwIpqs+2db7lON5KYwTg5CEknQh0R0Jomrqq/tko/WQPTYq+uDvHKXaB1NU1jsGi8ske/5DTIk1bBOupVaAVGwXY+3QXTcx20plQOsH+G2OgPE5NZV1VUuy1LkXohamsHYdZoFE+YkBO+eATXEm01HcFgoemHv58P4DBWhYEGGan58v4KGSJ+zT5ywz5NbMVYbglM2NV/UJqrvm+NJ/YFfvNEd3X1AOooAAAAASUVORK5CYII=",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAbFBMVEX////8/Pz09PT19fX4+Pjd3d2ampoAAAAzNDWoqKno6Oh0dHRcXFzIycm5ubktLi4hIiN7e3yLi4tmZ2fi4uI9Pj7S0tIHCQuzs7NJSkru7u6goKB+f3/W1taNjo69vb1PT1BsbW0VFRc3ODhehn9TAAAA30lEQVR4Ad3QhXHEMBBA0W8QmAVmTq7/HjOeDdSQewNiLfBukjRNM4A0TeCR/0wSpY0xtiihMgWPsraNnLWd8yFEE3vyOIwAeprhMQ5Lm8O6uT3hWPwKjQspj+zsLvlBuwNmV1DaukS+t3sqs8MWkAU7ejMiGnMhkjoCqnZmQ3CZPwM8Hy/8Hd7xF7BWzjQIxulCsJaQbG6r7xXR2zNHrn1s0AwhUdNPjunejTLxTlHedS89EOMwFAkobzR4ewFptRyI9tPY18uZkFIYzUO5akWU22BflYKx2xGX1Rn/yxf+uwzyBt/vSwAAAABJRU5ErkJggg==",
+      },
+      {
+        name: "Java",
+        image: "https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg"
+      },
+      {
+        name: "Python",
+        image: "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg"
+      },
+      {name: "Django",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/75/Django_logo.svg"
+      },
+      {
+        name: "Spring Boot",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/79/Spring_Boot.svg"
+      },
+      {
+        name: "Oracle",
+        image: "https://upload.wikimedia.org/wikipedia/commons/5/50/Oracle_logo.svg"
+      },
+      {
+        name: "PostgreSQL",
+        image: "https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg"
+      },
+      {
+        name: "MySQL",
+        image: "https://upload.wikimedia.org/wikipedia/en/d/dd/MySQL_logo.svg"
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "AWS",
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg"
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image: "https://upload.wikimedia.org/wikipedia/en/f/f4/Docker_logo.svg"
+      },
+      {
+        name: "Kubernetes",
+        image: "https://upload.wikimedia.org/wikipedia/commons/3/39/Kubernetes_logo_without_workmark.svg"
+      },
+      {
+        name: "Jira",
+        image: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Jira_Logo.svg"
+      },
+      {
+        name: "BitBucket",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/44_Bitbucket_logo_logos-512.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image: "https://cdn.worldvectorlogo.com/logos/postman.svg",
+      },
+      {
+        name: "Swagger",
+        image:
+          "https://static-00.iconduck.com/assets.00/swagger-icon-1024x1024-09037v1r.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1166690750/85f69649-5387-44c2-ba45-81ae13812e36-cover.png",
+      },
+      {
+        name: "Jenkins",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAABHVBMVEX////39/eZmZtVUlGFeW2Admtxbm3Cw8W4t7h0cnSonZD65Mr54cSynIPy2bvq0bPw1rd1aFvs7OxbWFiLioy2pJDJs5nz2bnSu5+EfXiqqquCf4BeXmPZ3d1DAABJSlDgx6reyrKfjnzAq5NqXE5/GRmWSEOrnYxnX1dyAADpLCiQXlaPgHDaJiJvXl6UDAnaMy+rIiGbloGib2GeCgDMJSDUODOzFxidjHmbIyOEc3OOgIAgLz9ZWE0bSl9SKiyAXV27MixHNDo5bIQ/b4chOkmHjZCQKCJRNDF0Wk95NDd6KiiCaGgzUGGsMjB5MDJrOT9SZnKMpq9PdYQaQldvTk4zS1uHvtVXR1NeAgBBHigmMToWOUwFGygAIzhANql4AAABWUlEQVR4AayRAxIDMRRAa66VrW0ta9u27n+LalCOGycv34q/NKVKrdHq9D+ZwWgyQxAEq34zCEExDMK/ZQmSohENAzCUtXxBq83OoBiCohgKfzKHU+1yA8zjxWif/0OvMhAMhX0AsCygWV+EeIO2aMyNAgwDAIHicCL5JpjieMF3g4jb66VFTnK8hiGn0xkTcAOPN+tlc2n5NVYqkU5zMTPDQr6sG+fSXP4FFm73XDEEQgLKlMqVNFd9leRq9Uaz1e7w3V663xhww9cMyPXmaDQqjzm+n5ncNuo1BVKncYOj6XjmmC+W6cxbGlbyDa1H68WmPR9NE/337EnbOywXWzPXcje0vedPtR+NDm2rQU+1KIODMLxT22h0fOg4GvQOpepDdDk6Oh6QtFGqz7pQp/P9iXAZFARxHWb6YmcCSUpxYE9+YJtk2LFLynJKSYlI4Uy6QkKMxKZyAGgaMOiIUjjnAAAAAElFTkSuQmCC",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/commons/5/56/Deloitte.svg",
+    role: "Staff Software  Engineer",
+    company: "Deloitte",
+    date: "Apr 2022 - Aug 2024",
+    desc: `
+      ● Built a high-efficiency solution for pharmaceutical trials, using ReactJS, NodeJS, Serverless, REST API, JavaScript, PostgreSQL, 
+      and AWS, which reduced trial efforts by 50% and cut processing time by 40%, earning the Applause Award.
+      ● Integrated end-to-end courier logistics, using NodeJS, Serverless, and AWS, significantly enhancing delivery reliability 
+      and decreasing shipping errors by up to 25%.
+      ● Deployed open-source Apache Superset for data visualization using Python, saving $15,000 annually in software 
+      licensing costs and resulting in substantial improvements in key performance metrics.
+    `,
+    skills: [
+      "ReactJS",
+      "NextJS",
+      "JavaScript",
+      "TypeScript",
+      "Redux Toolkit",
+      "HTML",
+      "CSS",
+      "Jest",
+      "NodeJS",
+      "PostgreSQL",
+      "Serverless",
+      "Kubernetes",
+      "AWS",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://upload.wikimedia.org/wikipedia/commons/0/04/Datamatics_Logo.svg",
+    role: "Software Engineer",
+    company: "Datamatics",
+    date: "Mar 2021 - Mar 2022",
+    desc: `● Created backend functionalities using Node.js, Express.js, and AWS to improve system reliability and handle a daily 
+user base of 50,000 active users.
+● Enhanced system scalability using AWS, which increased system efficiency by 2x and allowed seamless performance 
+during peak loads.
+● Developed a dashboard using React, Node.js, and JavaScript to automate agent onboarding, reducing the process 
+time from 1 hour to under 10 minutes, significantly increasing efficiency and supporting network expansion.`,
+    skills: [
+      "ReactJS",
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "Jest",
+      "NodeJS",
+      "MySQL",
+      "ExpressJS",
+      "AWS",
+      "Docker",
+      "Git",
+    ],
+  },
+  {
+    id: 3,
+    img: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Hexagon_AB_Logo_Color.svg",
+    role: "Software Engineer",
+    company: "Hexagon Global",
+    date: "Aug 2019 - Mar 2021",
+    desc: `● Created a Report Designer feature using Java and MySQL, simplifying the report creation process and reducing the 
+time from 2-3 hours to under 30 minutes, resulting in enhanced efficiency and improved workflow.
+● Implemented ETL processes using Java for data integration and transformation, enhancing data processing efficiency 
+by 25%.
+● Troubleshot and debugged back-end issues in Java-based systems, optimizing MySQL queries to reduce system 
+downtime by 40% and improve database performance.`,
+    skills: [
+      "Java",
+      "JavaScript",
+      "MySQL",
+      "Spring Boot",
+      "HTML5",
+      "CSS3",
+      "Material Ui",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 1,
+    img: "https://upload.wikimedia.org/wikipedia/en/a/a1/Arizona_State_University_seal.svg",
+    school: "Arizona State University, Arizona",
+    date: "Aug 2024 - May 2026",
+    gpa: "4.00 GPA",
+    desc: "Currently pursuing a Master of Science in Data Science, Analytics, and Engineering with a focus on machine learning, data analysis, and scalable systems.",
+    degree: "Master of Science, Data Science, Analytics, and Engineering",
+  },  
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2Qkv-Yt0a2nG8aeuyLSRP3Ps5Jqyk3P14YA&s",
+    school: "Atria Institute of Technology, Bangalore",
+    date: "Aug 2014 - June 2018",
+    percentage: "7.2 CGPA",
+    desc: "I completed my bachelor of engineering degree from Atria Institute of Technology, Bangalore. Where i completed my degree in Computer Science and Engineering.",
+    degree: "Bachelor of Engineering, Computer Science and Engineering",
+  }
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "DoctorOnCall",
+    date: "Jun 2024 - Sept 2024",
+    description:
+      `● Engineered an advanced doctor appointment platform using React, Node.js, and Prisma, offering flexible scheduling 
+and intelligent doctor matching, reducing appointment scheduling time by 50%.
+● Integrated automated notifications and intuitive dashboards, leading to a 40% increase in user satisfaction and 
+streamlined management.`,
+    image:
+      "https://www.doctoroncall.com/marketplace/img/logo/B2B-Logo.png",
+    tags: [
+      "React.js",
+       "Node.js",
+       "MySQL",
+      "TypeScript",
+      "JavaScript",
+      "Redux",
+      "Bootstrap",
+      "HTML5",
+      "CSS3",
+    ],
+    category: "web app",
+    webapp: "https://clouldplaydoctor.online/",
+    member: [
+      {
+        name: "Raj Kumar Mahto",
+        img: "https://avatars.githubusercontent.com/u/18094764?v=4",
+        linkedin: "https://www.linkedin.com/in/rajmahato111/",
+        github: "https://github.com/rajmahato111",
+      },
+    ],
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
